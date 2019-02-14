8bdc44e8dce86646ea375392a27d93ab928faf93
qingxin.zhu <17611251996@163.com>
2019-02-14 16:54:46 +0800

    Spring-Aop Track

---
+++ src/main/java/org/sc/controller/FirstController.java
@@ -0,0 +1,20 @@
+package org.sc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Sc on 2019/2/14.
+ *
+ * @Description:
+ */
+@RestController
+public class FirstController {
+
+    @RequestMapping("demo")
+    public void demo(){
+
+        System.out.println("demo");
+    }
+
+}
