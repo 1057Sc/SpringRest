f1df4840acdffff374ed01fb7449cafbefcbdb4e
sc1057 <13525233384@163.com>
2019-10-31 08:33:13 +0800

    daily commit

--- src/main/java/org/sc/demo/AppController.java
+++ src/main/java/org/sc/demo/AppController.java
@@ -2,10 +2,13 @@ package org.sc.demo;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.management.AttributeList;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Api(value = "应用服务",description = "提供应用所需的所有APi")
 @RestController
@@ -23,6 +26,17 @@ public class AppController {
     @GetMapping("/getkv")
     public String get(@RequestParam String key,@RequestParam String value){
         return key+value;
+    }
+
+
+    @ApiOperation("得到服务键值对")
+    @GetMapping("/demo/obj")
+    @ResponseBody
+    public Map get(String key){
+        Map map = new HashMap();
+        Object list = new ArrayList<>();
+        map.put("list",list);
+        return map;
     }
 
 }
