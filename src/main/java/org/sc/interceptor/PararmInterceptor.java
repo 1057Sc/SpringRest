efc96a453ae598876f062f27abe7793e3b48198e
sc1057 <13525233384@163.com>
2019-11-01 16:17:28 +0800

    read request body

--- src/main/java/org/sc/interceptor/PararmInterceptor.java
+++ src/main/java/org/sc/interceptor/PararmInterceptor.java
@@ -3,16 +3,90 @@ package org.sc.interceptor;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class PararmInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder("");
+        try {
+            br = request.getReader();
+            String str;
+            while ((str = br.readLine()) != null) {
+                sb.append(str);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != br) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
         String page = request.getParameter("page");
         System.out.println(page);
         return true;
     }
+
+    public static void ReadAsChars2(HttpServletRequest request) {
+        InputStream is = null;
+        try {
+            is = request.getInputStream();
+            StringBuilder sb = new StringBuilder();
+            byte[] b = new byte[4096];
+            for (int n; (n = is.read(b)) != -1; ) {
+                sb.append(new String(b, 0, n));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // 二进制读取
+    public static byte[] readAsBytes(HttpServletRequest request) {
+
+        int len = request.getContentLength();
+        byte[] buffer = new byte[len];
+        ServletInputStream in = null;
+
+        try {
+            in = request.getInputStream();
+            in.read(buffer, 0, len);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != in) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return buffer;
+    }
+
 }
