a6bbe4b49cdde4633fd719d97d1f38a48a95f467
1057Sc <13525233384@163.com>
2019-12-01 08:58:43 +0800

    get ip from http

---
+++ src/main/java/org/sc/util/IpAddress.java
@@ -0,0 +1,21 @@
+package org.sc.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class IpAddress {
+
+
+    private static String getClientIp(HttpServletRequest request) {
+
+        String remoteAddr = "";
+
+        if (request != null) {
+            remoteAddr = request.getHeader("X-FORWARDED-FOR");
+            if (remoteAddr == null || "".equals(remoteAddr)) {
+                remoteAddr = request.getRemoteAddr();
+            }
+        }
+
+        return remoteAddr;
+    }
+}
